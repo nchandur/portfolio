018db39789961e887d50392bdb9b4085d8ea6404
chandurnischal <chandur.nischal@gmail.com>
2024-09-23 11:28:55 -0400

    career fair update

--- src/assets/files/WorkDetails.js
+++ src/assets/files/WorkDetails.js
@@ -1,45 +1,45 @@
 const work = [
     {
-        name: "University of Maryland",
-        time: "August 2024 - Present",
-        location: "College Park, MD, USA",
-        role: "Teaching Assistant",
+        name: `University of Maryland`,
+        time: `August 2024 - Present`,
+        location: `College Park, MD, USA`,
+        role: `Teaching Assistant`,
         summary: `Principles of Data Science (Prof. Tammy Perrin) | Principles of Machine Learning (Prof. Alejandra Mercado)`,
     },
     {
-        name: "Ecolab Inc.",
-        time: "June 2024 - August 2024",
-        location: "Naperville, IL, USA",
-        role: "Data Science Graduate Intern",
-        summary: "I validated AI models for cooling tower monitoring systems by analyzing time series data from sensors measuring parameters like temperature and pH. I studied normal and anomalous data to evaluate model predictions and explored predictive techniques like ARIMAs and k-shape clustering. I built a validation dataset with known anomalies and developed an algorithm to generate synthetic sensor data, allowing users to customize anomaly duration, frequency, and length. This tool enabled extensive testing of various models."
+        name: `Ecolab Inc.`,
+        time: `June 2024 - August 2024`,
+        location: `Naperville, IL, USA`,
+        role: `Data Science Graduate Intern`,
+        summary: `I analyzed time series sensor data—temperature, pH, and conductivity—to validate AI models for cooling tower monitoring. Employing techniques like ARIMA and k-shape clustering, I identified critical operational patterns and anomalies. I crafted a robust validation dataset from historical data and company documentation, enhancing the precision of anomaly prediction. Additionally, I engineered a synthetic data generation algorithm that replicates real-world sensor behavior, empowering users to tailor anomaly characteristics for exhaustive testing of boundary conditions.`
     },
     {
-        name: "ReWorked.ai",
-        time: "April 2024 - May 2024",  
-        location: "College Park, MD, USA",
-        role: "Machine Learning Engineer",
-        summary: "I tackled the challenge of predicting whether residents would install solar panels by analyzing a dataset with demographic and structural variables. I conducted exploratory data analysis to understand how features like age, income, roof area, and sunlight exposure influenced the decision. I then divided the problem into two parts: using a Bayesian decision model for demographic features and a random forest classifier for structural features. I combined the outputs of these models to estimate the likelihood of solar panel installation. This approach enabled the company to target marketing efforts more effectively, saving time and resources."
+        name: `ReWorked.ai`,
+        time: `April 2024 - May 2024`,  
+        location: `College Park, MD, USA`,
+        role: `Machine Learning Engineer`,
+        summary: `I developed a machine learning algorithm to predict the likelihood of solar panel installation by analyzing key demographic and structural variables, including age, income, roof area, and sunlight exposure. Through exploratory data analysis and expert consultations, I crafted a hybrid model that integrates a Bayesian decision model for demographic data with an optimized random forest classifier for structural features, yielding a consolidated probability score. This innovative approach optimized our targeted marketing strategy, allowing the company to pinpoint high-potential neighborhoods, enhance resource allocation, and significantly reduce man-hours.`
     },
     {
-        name: "Latlong (ONZE Technologies Pvt. Ltd.)",
-        time: "September 2022 - June 2023",
-        location: "Bangalore, KA, India",
-        role: "Data Engineer",
-        summary: "I created user-friendly Python tools to extract and analyze location-based data, helping uncover hidden profit opportunities. By combining Python with QGIS, I was able to study and visually present client data alongside geo-spatial attributes, making complex information easy to understand. Through detailed statistical analysis, I estimated regional profitability, which has been incredibly useful for financial institutions, automobile companies, and real estate developers to make well-informed decisions."
+        name: `Latlong (ONZE Technologies Pvt. Ltd.)`,
+        time: `September 2022 - June 2023`,
+        location: `Bangalore, KA, India`,
+        role: `Data Engineer`,
+        summary: `I developed an advanced OCR data extraction tool using OpenCV, Pytesseract, and fuzzy string matching to extract, process demographic data from both English and non-English documents and organize it in a structured database for seamless future access. I built a geo-spatial visualization tool using Python and QGIS, empowering a non-banking financial company to pinpoint underperforming areas and uncover new market opportunities, ultimately enhancing loan approval rates and profitability. By applying this visualization tool to market analysis for an automobile company, I identified regions with significant competitor market share, delivering critical insights that boosted operational efficiency and profitability.`
     },
     {
-        name: "Latlong (ONZE Technologies Pvt. Ltd.)",
-        time: "January 2022 - July 2022",
-        location: "Bangalore, KA, India",
-        role: "Software Engineering Intern",
-        summary: "I developed an OCR data extraction tool using OpenCV and Pytesseract to extract demographic data from both English and non-English documents through fuzzy string matching. After processing and handling missing data, I structured it into a database for future use. Using this data along with geo-spatial information, I created a visualization tool with Python and QGIS. This tool helped a non-banking financial company analyze loan approval rates, identify poorly performing regions, and uncover market opportunities to increase profitability. Similarly, an automobile company used the tool to pinpoint areas where they lacked market share, understand competitive dynamics, and enhance their operations and profit."
+        name: `Latlong (ONZE Technologies Pvt. Ltd.)`,
+        time: `January 2022 - July 2022`,
+        location: `Bangalore, KA, India`,
+        role: `Software Engineering Intern`,
+        summary: `I developed robust OCR APIs using Tesseract to efficiently extract, process, and store data from public documents. By implementing a clustering algorithm, I identified regions in India with high purchasing power based on geo-spatial data, enabling targeted marketing strategies. Additionally, I utilized Python and QGIS for data visualization, providing clients with actionable insights to enhance their marketing efforts and optimize resource allocation.`
     },
     {
-        name: "MTRDC, Defence Research and Development Organization",
-        time: "August 2021 - December 2021",
-        location: "Bangalore, KA, India",
-        role: "Research Intern",
-        summary: "During my core electronics internship under Dr. Vishal Kesari, I delved into the workings of Radio Frequency (RF) windows and their crucial role in effective signal communication. I explored the bandwidth within which traditional RF windows operate and sought to enhance this bandwidth by making physical modifications to the incident surfaces of the windows. I proposed three different structures that significantly improved the bandwidths from 5.68% to 32.98%, 36.2%, and 45.06%. This research was published and presented at the Wireless Antenna & Microwave Symposium 2022 at NIT Rourkela, India."
+        name: `MTRDC, Defence Research and Development Organization`,
+        time: `August 2021 - December 2021`,
+        location: `Bangalore, KA, India`,
+        role: `Research Intern`,
+        summary: `During my core electronics internship under Dr. Vishal Kesari, I delved into the workings of Radio Frequency (RF) windows and their crucial role in effective signal communication. I explored the bandwidth within which traditional RF windows operate and sought to enhance this bandwidth by making physical modifications to the incident surfaces of the windows. I proposed three different structures that significantly improved the bandwidths from 5.68% to 32.98%, 36.2%, and 45.06%. This research was published and presented at the Wireless Antenna & Microwave Symposium 2022 at NIT Rourkela, India.`
     },
 ];
 

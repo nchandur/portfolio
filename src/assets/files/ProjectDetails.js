c2b2414d3ba726a228b894dd19afb9688196ab5a
nchandur <chandur.nischal@gmail.com>
2025-05-12 18:52:36 -0400

    updated resume

--- src/assets/files/ProjectDetails.js
+++ src/assets/files/ProjectDetails.js
@@ -5,7 +5,7 @@ const projectList = [
         time: `August 2024 - December 2024`,
         summary: `Explore Middle-earth with Lorekeeper, a locally running RAG model built on Tolkien's The Lord of the Rings and The Hobbit. With an intuitive front-end, it lets users dive into characters, events, and lore through AI-driven insights, merging technology with storytelling.`,
         image: `images/lorekeeper.jpg`,
-        link: `https://github.com/chandurnischal/lorekeeper`
+        link: `https://github.com/nchandur/lorekeeper`
     },
     {
         title: `Dimension Reduction Analysis`,
@@ -13,7 +13,7 @@ const projectList = [
         time: `April 2024 - May 2024`,
         summary: `Discover the art of simplifying complex data with this exploration of dimension reduction techniques. Using the Fruit-360 dataset, this project evaluates various data representation methods, analyzing their efficiency and suitability for different applications.`,
         image: `images/fruit-classification.png`,
-        link: `https://github.com/chandurnischal/fruit-classification`
+        link: `https://github.com/nchandur/fruit-classification`
     },
     {
         title: `Sign Language Recognition and Translation`,
@@ -21,7 +21,7 @@ const projectList = [
         time: `March 2024 - May 2024`,
         summary: `Unlock seamless communication with a custom CNN trained on American and Indian Sign Languages. This project enables users to upload gesture images for translation or learn to fingerspell any string, combining AI innovation with accessibility and learning.`,
         image: `images/sign-language.png`,
-        link: `https://github.com/chandurnischal/sign-language`
+        link: `https://github.com/nchandur/sign-language`
     },
     {
         title: `NBA Prediction & Analysis Model`,
@@ -29,7 +29,7 @@ const projectList = [
         time: `August 2023 - December 2023`,
         summary: `Step into the world of data-driven basketball predictions with this advanced machine learning model, powered by data from 1980 to 2024. Users can explore daily matchups, dive into team and player stats, and get AI-powered insights on game outcomes. Whether you're making predictions or simply exploring the stats, this tool brings a whole new level of engagement to basketball.`,
         image: `images/nba.jpg`,
-        link: `https://github.com/chandurnischal/NBA-prediction-model`
+        link: `https://github.com/nchandur/NBA-prediction-model`
     },
 ];
 

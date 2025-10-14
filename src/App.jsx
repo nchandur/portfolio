6d7292c7d28851bf87f9968eace960807f178adf
nchandur <chandur.nischal@gmail.com>
2025-10-14 15:53:08 -0400

    theme toggle and background

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import React from "react";
+import { Theme } from "./components/Theme";
+import { StarBackground } from "./components/StarBackground";
 
-import Navigation from "./components/Navigation/Navigation";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
-import Education from "./components/Education/Education";
-import Professional from "./components/Professional/Professional";
-import Skills from "./components/Skills/Skills";
-import Projects from "./components/Projects/Projects";
+function App() {
 
-import 'bootstrap/dist/css/bootstrap.css';
-import "./App.css";
+  return (
+    <>
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Navigation />
-        <Home />
-        <About />
-        <Professional />
-        <Skills />
-        <Projects />
-        <Education />
-        <Contact />
-        <Footer />
-      </div>
-    )
-  }
+      <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+        <Theme />
+
+        <StarBackground />
+
+
+      </div>;
+
+
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Theme.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react"
+import { Sun, Moon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+export const Theme = () => {
+    const [isDark, setIsDark] = useState(false);
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem("theme");
+
+        if (storedTheme === "dark") {
+            setIsDark(true);
+            document.documentElement.classList.add("dark")
+        } else {
+            localStorage.setItem("theme", "light")
+            setIsDark(false);
+        }
+
+    }, [])
+
+
+    const toggleTheme = () => {
+        if (isDark) {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light")
+            setIsDark(false);
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "dark")
+            setIsDark(true);
+        }
+    }
+
+
+
+    return <button
+        onClick={toggleTheme}
+        className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+            "focus:outline-hidden"
+        )}
+    >
+        {isDark ?
+            <Sun className="h-6 w-6 text-yellow-300" />
+            :
+            <Moon className="h-6 w-6 text-blue-900" />}
+    </button>
+
+}

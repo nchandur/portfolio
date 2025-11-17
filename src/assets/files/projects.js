7e42cf114aafe49d210cdcfb5666908cabeec648
nchandur <chandur.nischal@gmail.com>
2025-11-17 18:56:09 -0500

    updated projects

--- src/assets/files/projects.js
+++ src/assets/files/projects.js
@@ -1,6 +1,14 @@
 const projects = [
   {
     id: 1,
+    title: "Blackjack",
+    description: "Command line Blackjack game for Linux terminal",
+    image: "images/blackjack.jpg",
+    tags: ["Go"],
+    url: "https://github.com/nchandur/blackjack"
+  },
+  {
+    id: 2,
     title: "GoReads!",
     description: "Content-Based Book Recommendation System",
     image: "images/goreads.jpg",
@@ -8,7 +16,7 @@ const projects = [
     url: "https://github.com/nchandur/go-reads",
   },
   {
-    id: 2,
+    id: 3,
     title: "Infinity Ledger",
     description: "Debt Management and Expense Tracking Tool",
     image: "images/ledger.jpg",
@@ -16,7 +24,7 @@ const projects = [
     url: "https://github.com/nchandur/ledger",
   },
   {
-    id: 3,
+    id: 4,
     title: "Lorekeeper",
     description: "RAG-based specialist on the Lord of the Rings literature",
     image: "images/lorekeeper.jpg",
@@ -24,7 +32,7 @@ const projects = [
     url: "https://github.com/nchandur/lorekeeper",
   },
   {
-    id: 4,
+    id: 5,
     title: "NBA Prediction Model",
     description: "ML-Based Game Predictor for the NBA",
     image: "images/nba.jpg",
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,7 +15,7 @@ export const Projects = () => {
                     Here are some of my recent projects.
                 </p>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-10">
                     {projects.map((project, key) => (
                         <div
                             key={key}
